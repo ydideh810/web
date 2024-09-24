634abfbdd3c412cb595f0bfec4af68c274c5a500
ydideh810 <yd.baller810@gmail.com>
2024-09-23 23:51:19 -0400

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,11 +2,11 @@ import { SiteConfig } from "types";
 
 export const siteConfig: SiteConfig = {
   name: "NiddamHub",
-  title: "Build reliable multimodal LLM-powered apps for your team",
+  title: "NiddamHub: Build reliable multimodal LLM-powered apps for your team",
   description:
     "Open-source platform providing all the tools needed to build multimodal AI-powered applications utilizing LLMs and AI agents, perfect for creating AI chat interfaces and data extraction",
   url: "https://scoopika.com",
-  ogImage: "https://scoopika.com/images/scoopika-new.png",
+  ogImage: "https://i.ibb.co/7ygsFW8/nh.png",
   links: {
     twitter: "https://twitter.com/scoopika_",
     github: "https://github.com/scoopika",
